ac3020eb4547697dfac14f45c1b03c696d8ab36c
Alnervo <alexj17200@gmail.com>
2025-01-02 17:08:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const audioList = document.getElementById('audio-list');
 
     // Clé API et identifiant du dossier Google Drive
-    const API_KEY = AIzaSyCyL7Iv519prVIDXmWAWaR2t5GHFrK-oDk; // Remplacez par votre clé API
+    const API_KEY = "AIzaSyCyL7Iv519prVIDXmWAWaR2t5GHFrK-oDk"; // Remplacez par votre clé API
     const FOLDER_ID = "1zBaK9wbAKv8ZRSHpz0N6hoyhDU4l9vYQ" ; // Remplacez par l'ID du dossier partagé
 
     validateButton.addEventListener('click', function() {
